317d4f276c8f5dd0e0381476d18f122482474229
buliangd <buliangd@126.com>
2022-07-03 11:51:37 +0800

    [CHG]更新：换成add_test指令后可使用进入构建目录后使用ctest启动测试代码

---
+++ components/common/cjson/test/test_cjson.cpp
@@ -0,0 +1,13 @@
+/**@file  
+ * @note   All Copyright (c) reserved
+ * @brief 
+ *
+ * @author buliangd
+ * @date   2022/07/03
+ *
+ * @version
+ *  date       |version    |author     |message
+ *  :----      |:----      |:----      |:----
+ *  2022/07/03 |V1.0.0     |buliangd   |create file
+ * @warning  
+ */
